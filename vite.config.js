9ce8b4fcb0b11308bec2a0d4a92f1fb4a5d22d18
batoure <batoure496@gmail.com>
2026-04-16 23:27:58 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://relaxed-pudding-d7217b.netlify.app/assets/index-B9tN9Nzc.js'   // ← AJOUTEZ CETTE LIGNE
+  base: './'
 })
